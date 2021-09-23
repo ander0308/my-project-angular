208a67f7bb596c89eb2b7034e575459cda0925a5
Anderson Santana <ander0308@yahoo.com.br>
2021-09-22 22:32:31 -0300

    inicio projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { headerComponent } from './headerComponent/headerComponent';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, headerComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/headerComponent/headerComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-component',
+  templateUrl: './headerComponent.html',
+  styleUrls: ['./headerComponent.css'],
+})
+export class headerComponent {}
