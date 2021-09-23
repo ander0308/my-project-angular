01312a2f80746296fb334b5233062c4b9b1ff704
Anderson <ander0308@yahoo.com.br>
2021-09-22 23:55:34 -0300

    desenvolvido componentes header e hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { headerComponent } from './headerComponent/headerComponent';
+import { heroComponent } from './heroComponent/heroComponent';
 
 @NgModule({
-  declarations: [AppComponent, headerComponent],
+  declarations: [AppComponent, headerComponent, heroComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/heroComponent/heroComponent.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hero-component',
+  templateUrl: './heroComponent.html',
+  styleUrls: ['./heroComponent.css'],
+})
+export class heroComponent {}
